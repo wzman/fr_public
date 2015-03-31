a0470c81b32dd4353231b6e07f802876b393cae6
fredakilla <fredakilla@gmail.com>
2015-03-31 13:20:07 +0200

    update drawmodes example

--- Altona2/Altona2/Examples/graphics/DrawModes/main.cpp
+++ Altona2/Altona2/Examples/graphics/DrawModes/main.cpp
@@ -15,12 +15,202 @@
 /***                                                                      ***/
 /****************************************************************************/
 
+const sInt ic = 6 * 6;
+const sInt vc = 24;
+
+static const sVertexPNT cubeVertices[vc] =
+{
+    { -1, 1, -1, 0, 0, -1, 0, 0, },
+    { 1, 1, -1, 0, 0, -1, 1, 0, },
+    { 1, -1, -1, 0, 0, -1, 1, 1, },
+    { -1, -1, -1, 0, 0, -1, 0, 1, },
+
+    { -1, -1, 1, 0, 0, 1, 0, 0, },
+    { 1, -1, 1, 0, 0, 1, 1, 0, },
+    { 1, 1, 1, 0, 0, 1, 1, 1, },
+    { -1, 1, 1, 0, 0, 1, 0, 1, },
+
+    { -1, -1, -1, 0, -1, 0, 0, 0, },
+    { 1, -1, -1, 0, -1, 0, 1, 0, },
+    { 1, -1, 1, 0, -1, 0, 1, 1, },
+    { -1, -1, 1, 0, -1, 0, 0, 1, },
+
+    { 1, -1, -1, 1, 0, 0, 0, 0, },
+    { 1, 1, -1, 1, 0, 0, 1, 0, },
+    { 1, 1, 1, 1, 0, 0, 1, 1, },
+    { 1, -1, 1, 1, 0, 0, 0, 1, },
+
+    { 1, 1, -1, 0, 1, 0, 0, 0, },
+    { -1, 1, -1, 0, 1, 0, 1, 0, },
+    { -1, 1, 1, 0, 1, 0, 1, 1, },
+    { 1, 1, 1, 0, 1, 0, 0, 1, },
+
+    { -1, 1, -1, -1, 0, 0, 0, 0, },
+    { -1, -1, -1, -1, 0, 0, 1, 0, },
+    { -1, -1, 1, -1, 0, 0, 1, 1, },
+    { -1, 1, 1, -1, 0, 0, 0, 1, },
+};
+
+static const sU16 cubeIndices[ic] =
+{
+    0, 1, 2, 0, 2, 3,
+    4, 5, 6, 4, 6, 7,
+    8, 9, 10, 8, 10, 11,
+    12, 13, 14, 12, 14, 15,
+    16, 17, 18, 16, 18, 19,
+    20, 21, 22, 20, 22, 23,
+};
+
+static const sVertexPNT triangleVertices[] =
+{      // pos                   // normal               // uv
+    { -0.5f, -0.5f,  0.0f,      0.0f,  0.0f, -1.0f,     0.0f, 0.0f,  },     // left-bottom
+    {  0.5f, -0.5f,  0.0f,      0.0f,  0.0f, -1.0f,     1.0f, 0.0f,  },     // right-bottom
+    {  0.0f,  0.5f,  0.0f,      0.0f,  0.0f, -1.0f,     0.5f, 1.0f,  }      // center-top
+};
+
+/****************************************************************************/
+/***                                                                      ***/
+/***                                                                      ***/
+/***                                                                      ***/
+/****************************************************************************/
+
 void Altona2::Main()
 {
-  sInt flags = 0;
+    sInt flags = 0;
     flags |= sSM_NoVSync;
     flags |= sSM_Debug;
-  sRunApp(new App,sScreenMode(flags,"DrawModes",1280,720));
+    sRunApp(new App, sScreenMode(flags, "DrawModes", 1280, 720));
+}
+
+/****************************************************************************/
+/***                                                                      ***/
+/***                                                                      ***/
+/***                                                                      ***/
+/****************************************************************************/
+
+DrawerBase::DrawerBase(sAdapter * Adapter)
+{
+    this->Adapter = Adapter;
+    this->Context = Adapter->ImmediateContext;
+
+    Mtrl = 0;
+    Tex = 0;
+
+    const sInt sx = 64;
+    const sInt sy = 64;
+    sU32 tex[sy][sx];
+    for (sInt y = 0; y < sy; y++)
+    for (sInt x = 0; x < sx; x++)
+        tex[y][x] = ((x^y) & 8) ? 0xffff8080 : 0xff80ff80;
+    Tex = new sResource(Adapter, sResTexPara(sRF_BGRA8888, sx, sy, 1), &tex[0][0], sizeof(tex));
+
+    Mtrl = new sMaterial(Adapter);
+    Mtrl->SetShaders(CubeShader.Get(0));
+    Mtrl->SetTexturePS(0, Tex, sSamplerStatePara(sTF_Linear | sTF_Clamp, 0));
+    Mtrl->SetState(sRenderStatePara(sMTRL_DepthOn | sMTRL_CullOn, sMB_Off));
+    Mtrl->Prepare(Adapter->FormatPNT);
+
+    cbv0 = new sCBuffer<CubeShader_cbvs>(Adapter, sST_Vertex, 0);
+}
+
+DrawerBase::~DrawerBase()
+{
+    delete Mtrl;
+    delete Tex;
+    delete cbv0;
+}
+
+/****************************************************************************/
+/***                                                                      ***/
+/***                                                                      ***/
+/***                                                                      ***/
+/****************************************************************************/
+
+Draw1::Draw1(sAdapter * Adapter) :
+    DrawerBase(Adapter)
+{
+}
+
+Draw1::~Draw1()
+{
+}
+
+void Draw1::Draw(const sTargetPara &tp)
+{
+    sU32 utime = sGetTimeMS();
+    sF32 time = utime*0.001f;
+
+    sVector41 scale(1,1,1);
+    sVector3 rot(0, time, 0);
+    sVector41 trans(0, 0, 0);
+
+    sViewport view;
+    view.Camera.k.w = -2;
+    view.Model = sSetSRT(scale, rot, trans);
+    view.ZoomX = 1 / tp.Aspect;
+    view.ZoomY = 1;
+    view.Prepare(tp);
+
+    cbv0->Map();
+    cbv0->Data->mat = view.MS2SS;
+    cbv0->Data->ldir.Set(-view.MS2CS.k.x, -view.MS2CS.k.y, -view.MS2CS.k.z);
+    cbv0->Unmap();
+
+    static const sU32 vfdesc[] =
+    {
+        /*sVF_Stream0 |*/ sVF_Position | sVF_F3,
+        /*sVF_Stream0 |*/ sVF_Normal | sVF_F3,
+        /*sVF_Stream0 |*/ sVF_UV0 | sVF_F2,
+        sVF_End,
+    };
+
+    sVertexFormat * format = Adapter->CreateVertexFormat(vfdesc);
+    sDrawPara dp(1, 1, 1, Mtrl, cbv0);
+    dp.Flags |= sDF_Arrays;
+    dp.VertexArrayCount = 3;
+    dp.VertexArray = (void*)triangleVertices;
+    dp.VertexArrayFormat = format;
+
+    Context->Draw(dp);
+}
+
+/****************************************************************************/
+/***                                                                      ***/
+/***                                                                      ***/
+/***                                                                      ***/
+/****************************************************************************/
+
+Draw2::Draw2(sAdapter * Adapter) :
+    DrawerBase(Adapter)
+{
+    Geo = new sGeometry(Adapter);
+    Geo->SetIndex(sResBufferPara(sRBM_Index | sRU_Static, sizeof(sU16), ic), cubeIndices);
+    Geo->SetVertex(sResBufferPara(sRBM_Vertex | sRU_Static, sizeof(sVertexPNT), vc), cubeVertices);
+    Geo->Prepare(Adapter->FormatPNT, sGMP_Tris | sGMF_Index16, ic, 0, vc, 0);
+}
+
+Draw2::~Draw2()
+{
+}
+
+void Draw2::Draw(const sTargetPara &tp)
+{
+    sU32 utime = sGetTimeMS();
+    sF32 time = utime*0.01f;
+
+    sViewport view;
+    view.Camera.k.w = -4;
+    view.Model = sEulerXYZ(time*0.3f, time*0.2f, time*0.1f);
+    view.ZoomX = 1 / tp.Aspect;
+    view.ZoomY = 1;
+    view.Prepare(tp);
+
+    cbv0->Map();
+    cbv0->Data->mat = view.MS2SS;
+    cbv0->Data->ldir.Set(-view.MS2CS.k.x, -view.MS2CS.k.y, -view.MS2CS.k.z);
+    cbv0->Unmap();
+
+    Context->Draw(sDrawPara(Geo, Mtrl, cbv0));
 }
 
 /****************************************************************************/
@@ -31,9 +221,9 @@ void Altona2::Main()
 
 App::App()
 {
-  Mtrl = 0;
-  Tex = 0;
-  Geo = 0;
+    DrawMode = 0;
+    for (int i = 0; i < MAXMODE; i++)
+        Drawer[i] = 0;
 }
 
 App::~App()
@@ -42,172 +232,71 @@ App::~App()
 
 void App::OnInit()
 {
-  Screen = sGetScreen();
-  Adapter = Screen->Adapter;
-  Context = Adapter->ImmediateContext;
+    Screen = sGetScreen();
+    Adapter = Screen->Adapter;
+    Context = Adapter->ImmediateContext;
 
-  const sInt sx=64;
-  const sInt sy=64;
-  sU32 tex[sy][sx];
-  for(sInt y=0;y<sy;y++)
-    for(sInt x=0;x<sx;x++)
-      tex[y][x] = ((x^y)&8) ? 0xffff8080 : 0xff80ff80;
-  Tex = new sResource(Adapter,sResTexPara(sRF_BGRA8888,sx,sy,1),&tex[0][0],sizeof(tex));
+    Drawer[0] = new Draw1(Adapter);
+    Drawer[1] = new Draw2(Adapter);
 
 
-  Mtrl = new sMaterial(Adapter);
-  Mtrl->SetShaders(CubeShader.Get(0));
-  Mtrl->SetTexturePS(0,Tex,sSamplerStatePara(sTF_Linear|sTF_Clamp,0));
-  Mtrl->SetState(sRenderStatePara(sMTRL_DepthOn|sMTRL_CullOn,sMB_Off));
-  Mtrl->Prepare(Adapter->FormatPNT);
-
-
-  const sInt ic = 6*6;
-  const sInt vc = 24;
-  static const sU16 ib[ic] =
-  {
-     0, 1, 2, 0, 2, 3,
-     4, 5, 6, 4, 6, 7,
-     8, 9,10, 8,10,11,
-
-    12,13,14,12,14,15,
-    16,17,18,16,18,19,
-    20,21,22,20,22,23,
-  };
-
-  static const sVertexPNT vb[vc] =
-  {
-    { -1, 1,-1,   0, 0,-1, 0,0, },
-    {  1, 1,-1,   0, 0,-1, 1,0, },
-    {  1,-1,-1,   0, 0,-1, 1,1, },
-    { -1,-1,-1,   0, 0,-1, 0,1, },
-
-    { -1,-1, 1,   0, 0, 1, 0,0, },
-    {  1,-1, 1,   0, 0, 1, 1,0, },
-    {  1, 1, 1,   0, 0, 1, 1,1, },
-    { -1, 1, 1,   0, 0, 1, 0,1, },
-
-    { -1,-1,-1,   0,-1, 0, 0,0, },
-    {  1,-1,-1,   0,-1, 0, 1,0, },
-    {  1,-1, 1,   0,-1, 0, 1,1, },
-    { -1,-1, 1,   0,-1, 0, 0,1, },
-
-    {  1,-1,-1,   1, 0, 0, 0,0, },
-    {  1, 1,-1,   1, 0, 0, 1,0, },
-    {  1, 1, 1,   1, 0, 0, 1,1, },
-    {  1,-1, 1,   1, 0, 0, 0,1, },
-
-    {  1, 1,-1,   0, 1, 0, 0,0, },
-    { -1, 1,-1,   0, 1, 0, 1,0, },
-    { -1, 1, 1,   0, 1, 0, 1,1, },
-    {  1, 1, 1,   0, 1, 0, 0,1, },
-
-    { -1, 1,-1,  -1, 0, 0, 0,0, },
-    { -1,-1,-1,  -1, 0, 0, 1,0, },
-    { -1,-1, 1,  -1, 0, 0, 1,1, },
-    { -1, 1, 1,  -1, 0, 0, 0,1, },
-  };
-
-  /*Geo = new sGeometry(Adapter);
-  Geo->SetIndex(sResBufferPara(sRBM_Index|sRU_Static,sizeof(sU16),ic),ib);
-  Geo->SetVertex(sResBufferPara(sRBM_Vertex|sRU_Static,sizeof(sVertexPNT),vc),vb);
-  Geo->Prepare(Adapter->FormatPNT,sGMP_Tris|sGMF_Index16);*/
-
-  cbv0 = new sCBuffer<CubeShader_cbvs>(Adapter,sST_Vertex,0);
-
-  DPaint = new sDebugPainter(Adapter);
+    DPaint = new sDebugPainter(Adapter);
 }
 
 void App::OnExit()
 {
-  delete DPaint;
-  delete Mtrl;
-  delete Tex;
-  delete Geo;
-  delete cbv0;
+    for (int i = 0; i < MAXMODE; i++)
+    {
+        if (Drawer[i])
+            delete Drawer[i];
+    }
+    delete DPaint;
 }
 
 void App::OnFrame()
 {
 }
 
-/*static const sVertexPNT vbs[] =
-{
-    { -1, 1, -1,  0, 0, -1, 0, 0, },
-    { 1, 1, -1,   0, 0, -1, 1, 0, },
-    { 1, -1, -1,  0, 0, -1, 1, 1, },
-    { -1, -1, -1, 0, 0, -1, 0, 1, }
-};*/
-
-static const float vbs[] = {
-
-    // First triangle
-    // Positions          // Colors           // Texture Coords
-    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
-    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Left
-    // Second triangle  /*         */
-    0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
-    0.5f, -0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
-    -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Left
-};
-
-float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
-};
-
 void App::OnPaint()
 {
-  sU32 utime = sGetTimeMS();
-  sF32 time = utime*0.01f;
-  sTargetPara tp(sTAR_ClearAll,0xff405060,Screen);
-  sViewport view;
+    sTargetPara tp(sTAR_ClearAll, 0xff405060, Screen);
 
-  Context->BeginTarget(tp);
+    Context->BeginTarget(tp);
 
-  view.Camera.k.w = -4;
-  view.Model = sEulerXYZ(time*0.11f,time*0.13f,time*0.15f);
-  view.ZoomX = 1/tp.Aspect;
-  view.ZoomY = 1;
-  view.Prepare(tp);
+    if (Drawer[DrawMode])
+            Drawer[DrawMode]->Draw(tp);
 
-  cbv0->Map();
-  cbv0->Data->mat = view.MS2SS;
-  cbv0->Data->ldir.Set(-view.MS2CS.k.x,-view.MS2CS.k.y,-view.MS2CS.k.z);
-  cbv0->Unmap();
+    DPaint->PrintFPS();
+    DPaint->PrintStats();
+    DPaint->Draw(tp);
 
- // Context->Draw(sDrawPara(Geo,Mtrl,cbv0));
-
-  static const sU32 vfdesc[] =
-  {
-      sVF_Stream0 | sVF_Position | sVF_F3,
-      sVF_Stream0 | sVF_Normal | sVF_F3,
-      sVF_Stream0 | sVF_UV0 | sVF_F2,
-      sVF_End,
-  };
-
-  sVertexFormat * format = Adapter->CreateVertexFormat(vfdesc);
-  sDrawPara dp(1,1,1, Mtrl, cbv0);
-  dp.Flags |= sDF_Arrays;
-  dp.VertexArrayCount = 6;
-  dp.VertexArray = (void*)vbs;
-  dp.VertexArrayFormat = format;
-
-  Context->Draw(dp);
-
-  DPaint->PrintFPS();
-  DPaint->PrintStats();
-  DPaint->Draw(tp);
-
-  Context->EndTarget();
+    Context->EndTarget();
 }
 
 void App::OnKey(const sKeyData &kd)
 {
-  if((kd.Key&(sKEYQ_Mask|sKEYQ_Break))==27)
-    sExit();
+    sU32 key = sCleanKeyCode(kd.Key);
+    switch (key)
+    {
+    case sKEY_Escape:
+    case sKEY_Escape | sKEYQ_Shift:
+        sExit();
+
+    case sKEY_F1:
+    case sKEY_F2:
+    case sKEY_F3:
+    case sKEY_F4:
+    case sKEY_F5:
+    case sKEY_F6:
+    case sKEY_F7:
+    case sKEY_F8:
+    case sKEY_F9:
+    case sKEY_F10:
+    case sKEY_F11:
+    case sKEY_F12:
+        DrawMode = key - sKEY_F1;
+        break;
+    }
 }
 
 void App::OnDrag(const sDragData &dd)
--- Altona2/Altona2/Examples/graphics/DrawModes/main.hpp
+++ Altona2/Altona2/Examples/graphics/DrawModes/main.hpp
@@ -1,19 +1,21 @@
 /****************************************************************************/
 /***                                                                      ***/
-/***   (C) 2012-2014 Dierk Ohlerich et al., all rights reserved.          ***/
+/***   (C) 2012-2014 all rights reserved.                                 ***/
 /***                                                                      ***/
 /***   Released under BSD 2 clause license, see LICENSE.TXT               ***/
 /***                                                                      ***/
 /****************************************************************************/
 
-#ifndef FILE_ALTONA2_EXAMPLES_GRAPHICS_CUBE_MAIN_HPP
-#define FILE_ALTONA2_EXAMPLES_GRAPHICS_CUBE_MAIN_HPP
+#ifndef FILE_ALTONA2_EXAMPLES_GRAPHICS_DRAW_MODE_HPP
+#define FILE_ALTONA2_EXAMPLES_GRAPHICS_DRAW_MODE_HPP
 
 #include "Altona2/Libs/Base/Base.hpp"
 #include "Altona2/Libs/Util/DebugPainter.hpp"
 #include "shader.hpp"
 
 using namespace Altona2;
+
+class DrawerBase;
 
 /****************************************************************************/
 /***                                                                      ***/
@@ -23,28 +25,63 @@ using namespace Altona2;
 
 class App : public sApp
 {
-  sMaterial *Mtrl;
-  sResource *Tex;
-  sGeometry *Geo;
-  sCBuffer<CubeShader_cbvs> *cbv0;
-  sDebugPainter *DPaint;
+    static const uint MAXMODE = 10;
+    sInt DrawMode;
+    DrawerBase * Drawer[MAXMODE];
 
-  sAdapter *Adapter;
-  sContext *Context;
-  sScreen *Screen;
+    sDebugPainter *DPaint;
+    sAdapter *Adapter;
+    sContext *Context;
+    sScreen *Screen;
 public:
-  App();
-  ~App();
+    App();
+    ~App();
 
-  void OnInit();
-  void OnExit();
-  void OnFrame();
-  void OnPaint();
-  void OnKey(const sKeyData &kd);
-  void OnDrag(const sDragData &dd);
+    void OnInit();
+    void OnExit();
+    void OnFrame();
+    void OnPaint();
+    void OnKey(const sKeyData &kd);
+    void OnDrag(const sDragData &dd);
 };
 
 /****************************************************************************/
 
-#endif  // FILE_ALTONA2_EXAMPLES_GRAPHICS_CUBE_MAIN_HPP
+class DrawerBase
+{
+public:
+    sMaterial *Mtrl;
+    sResource *Tex;
+    sCBuffer<CubeShader_cbvs> *cbv0;
+    sAdapter *Adapter;
+    sContext *Context;
+
+public:
+    DrawerBase(sAdapter * Adapter);
+    ~DrawerBase();
+    virtual void Draw(const sTargetPara &tp) = 0;
+};
+
+/****************************************************************************/
+
+class Draw1 : public DrawerBase
+{
+public:
+    Draw1(sAdapter * Adapter);
+    ~Draw1();
+    void Draw(const sTargetPara &tp);
+};
+
+/****************************************************************************/
+
+class Draw2 : public DrawerBase
+{
+    sGeometry *Geo;
+public:
+    Draw2(sAdapter * Adapter);
+    ~Draw2();
+    void Draw(const sTargetPara &tp);
+};
+
+#endif  // FILE_ALTONA2_EXAMPLES_GRAPHICS_DRAW_MODE_HPP
 
